196b051dcfb67b15a5a643c0979dc6bb0c5c3b2c
Aliaksandr Kalenik <kalenik.aliaksandr@gmail.com>
2025-12-12 12:24:45 +0100

    LibMedia: Add IncrementallyPopulatedStream

---
+++ Libraries/LibMedia/IncrementallyPopulatedStream.cpp
@@ -0,0 +1,93 @@
+/*
+ * Copyright (c) 2025, Aliaksandr Kalenik <kalenik.aliaksandr@gmail.com>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include <AK/RefPtr.h>
+#include <LibMedia/IncrementallyPopulatedStream.h>
+
+namespace Media {
+
+NonnullRefPtr<IncrementallyPopulatedStream> IncrementallyPopulatedStream::create_empty()
+{
+    return adopt_ref(*new IncrementallyPopulatedStream({}, false));
+}
+
+NonnullRefPtr<IncrementallyPopulatedStream> IncrementallyPopulatedStream::create_from_buffer(ByteBuffer&& data)
+{
+    return adopt_ref(*new IncrementallyPopulatedStream(move(data), true));
+}
+
+void IncrementallyPopulatedStream::append(ByteBuffer&& buffer)
+{
+    Threading::MutexLocker locker { m_mutex };
+    m_buffer.append(buffer);
+    m_state_changed.broadcast();
+}
+
+void IncrementallyPopulatedStream::close()
+{
+    Threading::MutexLocker locker { m_mutex };
+    m_closed = true;
+    m_state_changed.broadcast();
+}
+
+u64 IncrementallyPopulatedStream::expected_size() const
+{
+    if (m_closed)
+        return m_buffer.size();
+    return *m_expected_size;
+}
+
+void IncrementallyPopulatedStream::set_expected_size(u64 size)
+{
+    m_expected_size = size;
+    m_buffer.ensure_capacity(size);
+}
+
+DecoderErrorOr<size_t> IncrementallyPopulatedStream::read_at(size_t position, Bytes& bytes)
+{
+    Threading::MutexLocker locker { m_mutex };
+    while (position + bytes.size() > m_buffer.size() && !m_closed) {
+        m_state_changed.wait();
+    }
+
+    if (position >= m_buffer.size())
+        return DecoderError::with_description(DecoderErrorCategory::EndOfStream, "Blocking read reached end of stream"sv);
+    return m_buffer.bytes().slice(position).copy_trimmed_to(bytes);
+}
+
+DecoderErrorOr<void> IncrementallyPopulatedStream::Cursor::seek(size_t position, SeekMode mode)
+{
+    size_t new_position = m_position;
+
+    switch (mode) {
+    case SeekMode::SetPosition:
+        new_position = position;
+        break;
+    case SeekMode::FromCurrentPosition:
+        new_position += position;
+        break;
+    case SeekMode::FromEndPosition:
+        new_position = m_stream->expected_size() + position;
+        break;
+    default:
+        VERIFY_NOT_REACHED();
+    }
+
+    Bytes empty;
+    TRY(m_stream->read_at(new_position, empty));
+
+    m_position = new_position;
+    return {};
+}
+
+DecoderErrorOr<size_t> IncrementallyPopulatedStream::Cursor::read_into(Bytes& bytes)
+{
+    auto read_count = TRY(m_stream->read_at(m_position, bytes));
+    m_position += read_count;
+    return read_count;
+}
+
+}
